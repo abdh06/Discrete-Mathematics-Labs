0b525fce8b5c11d93a56e25334c5c9a215b99ff6
H <abdirahman.hersi2006@gmail.com>
2026-09-03 21:37:56 +0200

    Optimization attempt 1
    - Removed the large odd number loop from inner even loops.
    - Upgraded search method:
    
    Old search method:
    - loops through 5 unique even nums and adds them together for a sum
    - Finds odd sum by subtracting even_sum from 330
    - loops through 6 unique odd nums and adds them together to see if they become that odd sum
    - once found, checks if the combined 7 numbers becomes divisible by specified number and if they add up to 330
    - Checks if that list is in a list "solutions"
    - adds them to list (solutions)
    - adds Dsum to separate list
    
    New Search Method:
    - loops through 6 unique odd numbers to find their sum.
    - check if the odd_sum is inside the possible sums [50, 300]
    - adds odd_sum into a dict as a key that shows a list
    - appends odd_numbers as a list to that list
    
    - loops through 5 unique even numbers
    - gets even_sum
    - gets odd_sum from that even_sum
    - checks to see if odd_sum in the table
    - unpacks the odd_lists from that one sum one by one
    - adds them onto a list with the even numbers as a possible solution
    - checks divisibility by specified number
    - adds to set (solutions) and set (D_set)
    
    Co-Authored-By: D1laraSara <242975330+D1laraSara@users.noreply.github.com>

--- Lab1/threetwo.py
+++ Lab1/threetwo.py
@@ -1,11 +1,13 @@
 
+
+
 U = {i for i in range(1, 61)}
 
 def adv_subset_maker(st):
     # Make the final solution subsets into set S and D(S)
-    solutions = list()
-    D_sub = list()
-
+    solutions = set()
+    D_set = set()
+    sum_table = dict()
     # Create even and odd lists
     even = list()
     odd = list()
@@ -20,60 +22,86 @@ def adv_subset_maker(st):
     even = tuple(even)
     odd = tuple(odd)
 
-    # there are 5 even numbers, so we look for the number 330 - even_sum to find odd sum
+
+    # GENERATE A LIST OF SUMS
+    
+    # Loop through 6 unique numbers (if every number is bigger than the previous, it gurantees unique numbers, + that we go through all of the lists)
+    # After looping, add them together and turn it into a key for sum_table
+    # that key is then used to add other types of lists that adds to the same sum
+    # IMPORTANT LATER, PROMISE
+
+    for odd1 in odd:
+        for odd2 in odd:
+            if odd1 >= odd2:
+                continue
+            for odd3 in odd:
+                if odd1 >= odd3 or odd2 >= odd3:
+                    continue
+                for odd4 in odd:
+                    if odd1 >= odd4 or odd2 >= odd4 or odd3 >= odd4:
+                        continue
+                    for odd5 in odd:
+                        if odd1 >= odd5 or odd2 >= odd5 or odd3 >= odd5 or odd4 >= odd5:
+                            continue
+                        for odd6 in odd:
+                            if odd1 >= odd6 or odd2 >= odd6 or odd3 >= odd6 or odd4 >= odd6 or odd5 >= odd6:
+                                continue
+                            odd_sum = odd1 + odd2 + odd3 + odd4 + odd5 + odd6
+
+                            # Minimum Even_sum = 2 + 4 + 6 + 8 + 10  = 30
+                            # Maximum Even_sum = 60 + 58 + 56 + 54 + 52 = 280
+                            # That means:
+                            # smallest possible odd_sum = 330 - 280 = 50
+                            # largest possible odd_sum = 330 - 30 = 300
+                            # We discard anything else
+                            if odd_sum < 50 or odd_sum > 300:
+                               continue
+                            if odd_sum not in sum_table:
+                                sum_table[odd_sum] = []
+                            sum_table[odd_sum].append([odd1, odd2, odd3, odd4, odd5, odd6])
+
+    
+
+    # There are 5 even numbers, so we look for the number 330 - even_sum to find odd sum
 
     for even1 in even:
             even_sum = 0
             for even2 in even:
-                if even1 == even2:
+                if even1 >= even2:
                     continue
                 for even3 in even:
-                        if even1 == even3 or even3 == even2:
+                        if even1 >= even3 or even3 >= even2:
                               continue
                         for even4 in even:
-                                if even1 == even4 or even2 == even4 or even3 == even4:
+                                if even1 >= even4 or even2 >= even4 or even3 >= even4:
                                       continue
                                 for even5 in even:
-                                    if even1 == even5 or even2 == even5 or even3 == even5 or even4 == even5:
+                                    if even1 >= even5 or even2 >= even5 or even3 >= even5 or even4 >= even5:
                                         continue
                                     even_sum = even1 + even2 + even3 + even4 + even5
                                     evens = [even1, even2, even3, even4, even5]
     
-                                    # 6 Odds Sums = 330 - 5 Even Sums
+                                    # 6 Odds Sums = 330 - 5 Even Sums, Automatically turns the "Add to 330" condition true
                                     odd_sum = 330 - even_sum
-    
-                                    # 3. Check to see if the numbers fits the conditions (Odd sums cannot be negative, nor can it be even)
-                                    if odd_sum < 0 or odd_sum % 2 == 0:
-                                        continue
-                            # Skips the even_nums as we know this does NOT work
-    
-                        num1, num2, num3, num4, num5, num6 = 0,0,0,0,0,0
 
-                        for odd1 in odd:
-                            for odd2 in odd:
-                                if odd1 == odd2:
-                                    continue
-                                for odd3 in odd:
-                                    if odd1 == odd3 or odd2 == odd3:
+                                    # Check to see if the numbers fits the condition (Odd sums should exist in the lookup table)
+                                    if odd_sum not in sum_table:
                                         continue
-                                    for odd4 in odd:
-                                        if odd1 == odd4 or odd2 == odd4 or odd3 == odd4:
-                                            continue
-                                        for odd5 in odd:
-                                            if odd1 == odd5 or odd2 == odd5 or odd3 == odd5 or odd4 == odd5:
-                                                continue
-                                            for odd6 in odd:
-                                                if odd1 == odd6 or odd2 == odd6 or odd3 == odd6 or odd4 == odd6 or odd5 == odd6:
-                                                    continue
-                                                if odd1 + odd2 + odd3 + odd4 + odd5 + odd6 == odd_sum:
-                                                    odds = [odd1, odd2, odd3, odd4, odd5, odd6]
-                                                    possible_solution = sorted((*odds, *evens))
-                                                    if divisiblebynumber(possible_solution) and sumequals330(possible_solution):
-                                                        if possible_solution not in solutions:
-                                                            solutions.append(possible_solution)
-                                                            D_sub.append(abs(even_sum - odd_sum))
 
-    return solutions, D_sub
+                                    # LOGIC:
+                                    # Unpack one list at a time, add them together with the evens, check if they 
+                                    # are divisible by (2^6 * 3^4 * 5^2 * 7)
+
+                                    for odd_list in sum_table[odd_sum]:
+                                            possible_solution = tuple(sorted([*odd_list, *evens]))
+                                            if divisiblebynumber(possible_solution):
+                                                    solutions.add(possible_solution)
+                                                    D_set.add(abs(even_sum - odd_sum))
+    
+    solutions = tuple(sorted(solutions))
+    D_set = tuple(sorted(D_set))
+
+    return solutions, D_set
 
 
 def divisiblebynumber(subset):
@@ -92,7 +120,8 @@ def divisiblebynumber(subset):
 
     return False
 
-def sumequals330(subset):
+# Unused
+# def sumequals330(subset):
     if len(subset) != 11:
           return False
 
@@ -106,5 +135,7 @@ def sumequals330(subset):
     
     return False
 
-solutions, d_sub = adv_subset_maker(U)
-print(len(solutions))
+solutions, D_set = adv_subset_maker(U)
+smallest_dsum,largest_dsum = D_set[0], D_set[-1]
+lex_small, lex_large = solutions[0], solutions[-1]
+print(len(solutions), smallest_dsum, largest_dsum, lex_small, lex_large)
